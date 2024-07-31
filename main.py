a1e5ae6561cf1ebcc23bf4b41e451753b0e67d7f
Kilian <146135415+Masterx0202@users.noreply.github.com>
2024-07-31 13:41:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import customtkinter as ctk
-import temp as tmp
+import temperature as tmp
 
+def on_submit():
+    user_input = entry.get()
+    temp_value = tmp.temp(user_input)
+    label.configure(text="Temperature: " + str(temp_value) + "°C")
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
 
 root = ctk.CTk()
-root.title("CTkLabel Example")
+root.title("Weather")
 
-label = ctk.CTkLabel(root, text=tmp.temp("london"))
+
+entry = ctk.CTkEntry(root, width=300, placeholder_text="Enter your city here")
+entry.pack(pady=20)
+
+
+label = ctk.CTkLabel(root, text="")
 label.pack(pady=20)
+
+submit_button = ctk.CTkButton(root, text="Submit", command=on_submit)
+submit_button.pack(pady=10)
 
 root.mainloop()
